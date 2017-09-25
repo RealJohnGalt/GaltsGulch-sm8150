89cd84d01f0ee3f114ea5330204666cbb6e30afc
Rohit Gupta <rohgup@codeaurora.org>
2017-09-25 14:23:48 -0700

    cpufreq: schedutil: Cache tunables on governor exit
    Currently when all the related CPUs from a policy go offline or the
    governor is switched, cpufreq framework calls sugov_exit() that
    frees the governor tunables. When any of the related CPUs comes back
    online or governor is switched back to schedutil sugov_init() gets
    called which allocates a fresh set of tunables that are set to
    default values. This can cause the userspace settings to those
    tunables to be lost across governor switches or when an entire
    cluster is hotplugged out.
    To prevent this, save the tunable values on governor exit. Restore
    these values to the newly allocated tunables on governor init.
    
    Change-Id: I671d4d0e1a4e63e948bfddb0005367df33c0c249
    Signed-off-by: Rohit Gupta <rohgup@codeaurora.org>
    [avilaj@codeaurora.org: Modify to handle new tunables on 4.19]
    Signed-off-by: Jonathan Avila <avilaj@codeaurora.org>
    [Kazuki: Adapted for 5.4]
    Signed-off-by: Kazuki Hashimoto <kazukih@tuta.io>

--- kernel/sched/cpufreq_schedutil.c
+++ kernel/sched/cpufreq_schedutil.c
@@ -76,6 +76,7 @@ struct sugov_cpu {
 };
 
 static DEFINE_PER_CPU(struct sugov_cpu, sugov_cpu);
+static DEFINE_PER_CPU(struct sugov_tunables *, cached_tunables);
 
 /************************ Governor internals ***********************/
 
@@ -536,11 +537,46 @@ static struct attribute *sugov_attributes[] = {
 	NULL
 };
 
+static void sugov_tunables_save(struct cpufreq_policy *policy,
+		struct sugov_tunables *tunables)
+{
+	int cpu;
+	struct sugov_tunables *cached = per_cpu(cached_tunables, policy->cpu);
+
+	if (!have_governor_per_policy())
+		return;
+
+	if (!cached) {
+		cached = kzalloc(sizeof(*tunables), GFP_KERNEL);
+		if (!cached)
+			return;
+
+		for_each_cpu(cpu, policy->related_cpus)
+			per_cpu(cached_tunables, cpu) = cached;
+	}
+
+	cached->up_rate_limit_us = tunables->up_rate_limit_us;
+	cached->down_rate_limit_us = tunables->down_rate_limit_us;
+}
+
 static void sugov_tunables_free(struct kobject *kobj)
 {
 	struct gov_attr_set *attr_set = container_of(kobj, struct gov_attr_set, kobj);
 
 	kfree(to_sugov_tunables(attr_set));
+}
+
+static void sugov_tunables_restore(struct cpufreq_policy *policy)
+{
+	struct sugov_policy *sg_policy = policy->governor_data;
+	struct sugov_tunables *tunables = sg_policy->tunables;
+	struct sugov_tunables *cached = per_cpu(cached_tunables, policy->cpu);
+
+	if (!cached)
+		return;
+
+	tunables->up_rate_limit_us = cached->up_rate_limit_us;
+	tunables->down_rate_limit_us = cached->down_rate_limit_us;
 }
 
 static struct kobj_type sugov_tunables_ktype = {
@@ -691,6 +727,8 @@ static int sugov_init(struct cpufreq_policy *policy)
 	policy->governor_data = sg_policy;
 	sg_policy->tunables = tunables;
 
+	sugov_tunables_restore(policy);
+
 	ret = kobject_init_and_add(&tunables->attr_set.kobj, &sugov_tunables_ktype,
 				   get_governor_parent_kobj(policy), "%s",
 				   schedutil_gov.name);
@@ -730,8 +768,10 @@ static void sugov_exit(struct cpufreq_policy *policy)
 
 	count = gov_attr_set_put(&tunables->attr_set, &sg_policy->tunables_hook);
 	policy->governor_data = NULL;
-	if (!count)
+	if (!count) {
+		sugov_tunables_save(policy, tunables);
 		sugov_clear_global_tunables();
+	}
 
 	mutex_unlock(&global_tunables_lock);
 
