670ec56648fd678a8dceb07f09731121e71ebef2
Sami Tolvanen <samitolvanen@google.com>
2017-12-08 15:49:24 -0800

    FIXUP: ANDROID: init: ensure initcall ordering with LTO
    With LTO, the compiler doesn't necessarily obey link order for
    initcalls, and the initcall variable needs to be globally unique
    to avoid naming collisions.
    
    In order to preserve the correct order, we add each variable
    into its own section and generate a linker script (in
    scripts/link-vmlinux.sh) to ensure the order remains correct.  We
    also add a __COUNTER__ prefix to the name, so we can retain the
    order of initcalls within each compilation unit, and __LINE__ to
    make the names more unique.
    
    Bug: 145210207
    Change-Id: Iddda881a52b7942781713b188d810b6100159a2b
    Signed-off-by: Sami Tolvanen <samitolvanen@google.com>
    Signed-off-by: Danny Lin <danny@kdrag0n.dev>

--- include/linux/init.h
+++ include/linux/init.h
@@ -234,10 +234,7 @@ extern bool initcall_debug;
 	static exitcall_t __exitcall_##fn __exit_call = fn
 
 #define console_initcall(fn)	___define_initcall(fn, con, .con_initcall)
-
-#define security_initcall(fn)					\
-	static initcall_t __initcall_##fn			\
-	__used __section(.security_initcall.init) = fn
+#define security_initcall(fn)	___define_initcall(fn, security, .security_initcall)
 
 struct obs_kernel_param {
 	const char *str;
