64492243440fdc25e93ce79e3ba06b10ddcae582
Sami Tolvanen <samitolvanen@google.com>
2018-05-02 10:44:59 -0700

    FROMLIST: add support for Clang's Shadow Call Stack (SCS)
    This change adds generic support for Clang's Shadow Call Stack,
    which uses a shadow stack to protect return addresses from being
    overwritten by an attacker. Details are available here:
    
      https://clang.llvm.org/docs/ShadowCallStack.html
    
    Note that security guarantees in the kernel differ from the
    ones documented for user space. The kernel must store addresses
    of shadow stacks used by other tasks and interrupt handlers in
    memory, which means an attacker capable reading and writing
    arbitrary memory may be able to locate them and hijack control
    flow by modifying shadow stacks that are not currently in use.
    
    Bug: 145210207
    Change-Id: Ia5f1650593fa95da4efcf86f84830a20989f161c
    (am from https://lore.kernel.org/patchwork/patch/1149054/)
    Reviewed-by: Kees Cook <keescook@chromium.org>
    Reviewed-by: Miguel Ojeda <miguel.ojeda.sandonis@gmail.com>
    Signed-off-by: Sami Tolvanen <samitolvanen@google.com>
    Signed-off-by: Danny Lin <danny@kdrag0n.dev>

--- kernel/fork.c
+++ kernel/fork.c
@@ -94,6 +94,7 @@
 #include <linux/cpu_input_boost.h>
 #include <linux/devfreq_boost.h>
 #include <linux/simple_lmk.h>
+#include <linux/scs.h>
 
 // tedlin@ASTI 2019/06/12 add for CONFIG_HOUSTON
 #include <oneplus/houston/houston_helper.h>
@@ -387,6 +388,7 @@ void put_task_stack(struct task_struct *tsk)
 void free_task(struct task_struct *tsk)
 {
 	cpufreq_task_times_exit(tsk);
+	scs_release(tsk);
 
 #ifndef CONFIG_THREAD_INFO_IN_TASK
 	/*
@@ -683,6 +685,8 @@ void __init fork_init(void)
 			  NULL, free_vm_stack_cache);
 #endif
 
+	scs_init();
+
 	lockdep_init_task(&init_task);
 }
 
@@ -735,6 +739,10 @@ static struct task_struct *dup_task_struct(struct task_struct *orig, int node)
 	atomic_set(&tsk->stack_refcount, 1);
 #endif
 
+	if (err)
+		goto free_stack;
+
+	err = scs_prepare(tsk, node);
 	if (err)
 		goto free_stack;
 
