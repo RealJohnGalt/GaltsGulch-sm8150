91718c22d82fbd42959943b2ae2a813f34740142
Mathieu Desnoyers <mathieu.desnoyers@efficios.com>
2018-06-02 08:43:55 -0400

    arm64: rseq: Implement backend rseq calls and select HAVE_RSEQ
    Implement calls to rseq_signal_deliver, rseq_handle_notify_resume
    and rseq_syscall so that we can select HAVE_RSEQ on arm64.
    
    Acked-by: Mathieu Desnoyers <mathieu.desnoyers@efficios.com>
    Acked-by: Mark Rutland <mark.rutland@arm.com>
    Signed-off-by: Will Deacon <will.deacon@arm.com>

--- arch/arm64/kernel/ptrace.c
+++ arch/arm64/kernel/ptrace.c
@@ -1422,6 +1422,8 @@ asmlinkage void syscall_trace_exit(struct pt_regs *regs)
 
 	if (test_thread_flag(TIF_SYSCALL_TRACE))
 		tracehook_report_syscall(regs, PTRACE_SYSCALL_EXIT);
+
+	rseq_syscall(regs);
 }
 
 /*
