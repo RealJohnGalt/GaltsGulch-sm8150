55c437bb61fc468d0e90055738ef640a73c07e40
Will Deacon <will.deacon@arm.com>
2018-06-20 14:58:10 +0100

    asm-generic: unistd.h: Wire up sys_rseq
    The new rseq call arrived in 4.18-rc1, so provide it in the asm-generic
    unistd.h for architectures such as arm64.
    
    Acked-by: Arnd Bergmann <arnd@arndb.de>
    Acked-by: Mathieu Desnoyers <mathieu.desnoyers@efficios.com>
    Signed-off-by: Will Deacon <will.deacon@arm.com>

--- include/uapi/asm-generic/unistd.h
+++ include/uapi/asm-generic/unistd.h
@@ -732,9 +732,10 @@ __SYSCALL(__NR_pkey_alloc,    sys_pkey_alloc)
 __SYSCALL(__NR_pkey_free,     sys_pkey_free)
 #define __NR_statx 291
 __SYSCALL(__NR_statx,     sys_statx)
-
+#define __NR_rseq 293
+__SYSCALL(__NR_rseq, sys_rseq)
 #undef __NR_syscalls
-#define __NR_syscalls 292
+#define __NR_syscalls 293
 
 /*
  * All syscalls below here should go away really,
