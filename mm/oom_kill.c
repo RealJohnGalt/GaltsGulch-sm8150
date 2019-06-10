1a74936a363f3b20d0b23f0006b2633e7e7e9540
Tyler Nijmeh <tylernij@gmail.com>
2019-06-10 11:37:07 -0700

    mm: oom_kill: Do not dump tasks by default
    This takes RCU and tasks locks to simply print debugging information,
    which with certain log levels will not even display. DIsable this by
    default.
    
    Signed-off-by: Tyler Nijmeh <tylernij@gmail.com>

--- mm/oom_kill.c
+++ mm/oom_kill.c
@@ -52,7 +52,7 @@
 
 int sysctl_panic_on_oom;
 int sysctl_oom_kill_allocating_task;
-int sysctl_oom_dump_tasks = 1;
+int sysctl_oom_dump_tasks;
 
 DEFINE_MUTEX(oom_lock);
 /* Serializes oom_score_adj and oom_score_adj_min updates */
