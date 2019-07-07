85530c1015bc6f186c793ff9f4c3fe2dcb31ef41
Park Ju Hyung <qkrwngud825@gmail.com>
2019-07-08 02:37:30 +0900

    msm: kgsl: Disable snapshot, coresight and trace
    Signed-off-by: Park Ju Hyung <qkrwngud825@gmail.com>
    Signed-off-by: atndko <z1281552865@gmail.com>

--- drivers/gpu/msm/kgsl_events.c
+++ drivers/gpu/msm/kgsl_events.c
@@ -47,9 +47,6 @@ static void _kgsl_event_worker(struct work_struct *work)
 	struct kgsl_event *event = container_of(work, struct kgsl_event, work);
 	int id = KGSL_CONTEXT_ID(event->context);
 
-	trace_kgsl_fire_event(id, event->timestamp, event->result,
-		jiffies - event->created, event->func);
-
 	event->func(event->device, event->group, event->priv, event->result);
 
 	kgsl_context_put(event->context);
@@ -287,8 +284,6 @@ int kgsl_add_event(struct kgsl_device *device, struct kgsl_event_group *group,
 	event->group = group;
 
 	INIT_WORK(&event->work, _kgsl_event_worker);
-
-	trace_kgsl_register_event(KGSL_CONTEXT_ID(context), timestamp, func);
 
 	spin_lock(&group->lock);
 
