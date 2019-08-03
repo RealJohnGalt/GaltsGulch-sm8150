80cca42ecb0c2897e8c8cd59ce76dabc863e3b34
Tyler Nijmeh <tylernij@gmail.com>
2019-08-02 18:45:40 -0700

    block: Do not poll MQ block devices
    This adds unnecessary overhead to the block subsystem.
    
    Signed-off-by: Tyler Nijmeh <tylernij@gmail.com>

--- include/linux/blkdev.h
+++ include/linux/blkdev.h
@@ -661,7 +661,6 @@ struct request_queue {
 
 #define QUEUE_FLAG_MQ_DEFAULT	((1 << QUEUE_FLAG_STACKABLE)	|	\
 				 (1 << QUEUE_FLAG_SAME_COMP)	|	\
-				 (1 << QUEUE_FLAG_POLL))
 
 /*
  * @q->queue_lock is set while a queue is being initialized. Since we know
