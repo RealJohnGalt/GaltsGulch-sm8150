8c52adab9c458cd735c71987fd527560b5e83973
Vitaly Wool <vitalywool@gmail.com>
2019-10-10 23:09:15 +0300

    zpool: extend API to match zsmalloc
    This patch adds the following functions to the zpool API:
    - zpool_compact()
    - zpool_get_num_compacted()
    - zpool_huge_class_size()
    
    The first one triggers compaction for the underlying allocator, the
    second retrieves the number of pages migrated due to compaction for
    the whole time of this pool's existence and the third one returns
    the huge class size.
    
    This API extension is done to align zpool API with zsmalloc API.
    
    Signed-off-by: Vitaly Wool <vitalywool@gmail.com>
    Reviewed-by: Dan Streetman <ddstreet@ieee.org>
    Signed-off-by: Adam W. Willis <return.of.octobot@gmail.com>

--- include/linux/zpool.h
+++ include/linux/zpool.h
@@ -67,9 +67,7 @@ unsigned long zpool_get_num_compacted(struct zpool *pool);
 
 u64 zpool_get_total_size(struct zpool *pool);
 
-unsigned long zpool_compact(struct zpool *pool);
-
-unsigned long zpool_get_num_compacted(struct zpool *pool);
+size_t zpool_huge_class_size(struct zpool *zpool);
 
 /**
  * struct zpool_driver - driver implementation for zpool
@@ -84,7 +82,8 @@ unsigned long zpool_get_num_compacted(struct zpool *pool);
  * @unmap:	unmap a handle.
  * @compact:	try to run compaction over a pool
  * @get_num_compacted:	get amount of compacted pages for a pool
- * @total_size:	get total size of a pool.
+ * @total_size:	get total size of a pool
+ * @huge_class_size: huge class threshold for pool pages.
  *
  * This is created by a zpool implementation and registered
  * with zpool.
@@ -117,10 +116,7 @@ struct zpool_driver {
 	unsigned long (*get_num_compacted)(void *pool);
 
 	u64 (*total_size)(void *pool);
-
-	unsigned long (*compact)(void *pool);
-
-	unsigned long (*get_num_compacted)(void *pool);
+	size_t (*huge_class_size)(void *pool);
 };
 
 void zpool_register_driver(struct zpool_driver *driver);
