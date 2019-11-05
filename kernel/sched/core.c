8dec44ceca9c1286635371541fc41e6873f4841b
Qais Yousef <qais.yousef@arm.com>
2019-11-05 11:22:12 +0000

    UPSTREAM: sched/core: Fix compilation error when cgroup not selected
    When cgroup is disabled the following compilation error was hit
    
    	kernel/sched/core.c: In function ‘uclamp_update_active_tasks’:
    	kernel/sched/core.c:1081:23: error: storage size of ‘it’ isn’t known
    	  struct css_task_iter it;
    			       ^~
    	kernel/sched/core.c:1084:2: error: implicit declaration of function ‘css_task_iter_start’; did you mean ‘__sg_page_iter_start’? [-Werror=implicit-function-declaration]
    	  css_task_iter_start(css, 0, &it);
    	  ^~~~~~~~~~~~~~~~~~~
    	  __sg_page_iter_start
    	kernel/sched/core.c:1085:14: error: implicit declaration of function ‘css_task_iter_next’; did you mean ‘__sg_page_iter_next’? [-Werror=implicit-function-declaration]
    	  while ((p = css_task_iter_next(&it))) {
    		      ^~~~~~~~~~~~~~~~~~
    		      __sg_page_iter_next
    	kernel/sched/core.c:1091:2: error: implicit declaration of function ‘css_task_iter_end’; did you mean ‘get_task_cred’? [-Werror=implicit-function-declaration]
    	  css_task_iter_end(&it);
    	  ^~~~~~~~~~~~~~~~~
    	  get_task_cred
    	kernel/sched/core.c:1081:23: warning: unused variable ‘it’ [-Wunused-variable]
    	  struct css_task_iter it;
    			       ^~
    	cc1: some warnings being treated as errors
    	make[2]: *** [kernel/sched/core.o] Error 1
    
    Fix by protetion uclamp_update_active_tasks() with
    CONFIG_UCLAMP_TASK_GROUP
    
    Bug: 120440300
    Fixes: babbe170e053 ("sched/uclamp: Update CPU's refcount on TG's clamp changes")
    Reported-by: Randy Dunlap <rdunlap@infradead.org>
    Signed-off-by: Qais Yousef <qais.yousef@arm.com>
    Signed-off-by: Peter Zijlstra (Intel) <peterz@infradead.org>
    Tested-by: Randy Dunlap <rdunlap@infradead.org>
    Cc: Steven Rostedt <rostedt@goodmis.org>
    Cc: Ingo Molnar <mingo@redhat.com>
    Cc: Vincent Guittot <vincent.guittot@linaro.org>
    Cc: Patrick Bellasi <patrick.bellasi@matbug.net>
    Cc: Mel Gorman <mgorman@suse.de>
    Cc: Dietmar Eggemann <dietmar.eggemann@arm.com>
    Cc: Juri Lelli <juri.lelli@redhat.com>
    Cc: Ben Segall <bsegall@google.com>
    Link: https://lkml.kernel.org/r/20191105112212.596-1-qais.yousef@arm.com
    (cherry picked from commit e3b8b6a0d12cccf772113d6b5c1875192186fbd4)
    Signed-off-by: Qais Yousef <qais.yousef@arm.com>
    Change-Id: Ia4c0f801d68050526f9f117ec9189e448b01345a
    Signed-off-by: Quentin Perret <qperret@google.com>

--- kernel/sched/core.c
+++ kernel/sched/core.c
@@ -1069,6 +1069,7 @@ uclamp_update_active(struct task_struct *p, unsigned int clamp_id)
 	task_rq_unlock(rq, p, &rf);
 }
 
+#ifdef CONFIG_UCLAMP_TASK_GROUP
 static inline void
 uclamp_update_active_tasks(struct cgroup_subsys_state *css,
 			   unsigned int clamps)
@@ -1087,7 +1088,6 @@ uclamp_update_active_tasks(struct cgroup_subsys_state *css,
 	css_task_iter_end(&it);
 }
 
-#ifdef CONFIG_UCLAMP_TASK_GROUP
 static void cpu_util_update_eff(struct cgroup_subsys_state *css);
 static void uclamp_update_root_tg(void)
 {
