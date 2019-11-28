de69c2f45af9210220901082943b86b7519c823f
Adam W. Willis <return.of.octobot@gmail.com>
2019-11-27 21:08:05 -0500

    [FIXUP] 0b8fad8 / 5f6e060 ("cpu_input_boost: Allow configuration of each cluster's (minimum|idle) frequency")
    Well.. this is embarrassing. Correct cpu_perf_mask typos.
    
    Signed-off-by: Adam W. Willis <return.of.octobot@gmail.com>

--- drivers/cpufreq/cpu_input_boost.c
+++ drivers/cpufreq/cpu_input_boost.c
@@ -118,7 +118,7 @@ static unsigned int get_min_freq(struct cpufreq_policy *policy)
 
 	if (cpumask_test_cpu(policy->cpu, cpu_lp_mask))
 		freq = cpu_freq_min_little;
-	else if (cpumask_test_cpu(policy->cpu, cpu_lp_perf))
+	else if (cpumask_test_cpu(policy->cpu, cpu_perf_mask))
 		freq = cpu_freq_min_big;
 	else
 		freq = cpu_freq_min_prime;
@@ -132,7 +132,7 @@ static unsigned int get_idle_freq(struct cpufreq_policy *policy)
 
 	if (cpumask_test_cpu(policy->cpu, cpu_lp_mask))
 		freq = cpu_freq_idle_little;
-	else if (cpumask_test_cpu(policy->cpu, cpu_lp_perf))
+	else if (cpumask_test_cpu(policy->cpu, cpu_perf_mask))
 		freq = CONFIG_CPU_FREQ_IDLE_PERF;
 	else
 		freq = CONFIG_CPU_FREQ_IDLE_PERFP;
