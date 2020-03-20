13596e2705164c223ad79f46c0a30fbd49361f9c
Pavankumar Kondeti <pkondeti@codeaurora.org>
2020-03-20 13:36:54 +0530

    sched: Improve the scheduler
    This change is for general scheduler improvements.
    
    Change-Id: I3842c589378ce5bcc54e4c35ebcbfbdfa1f88a8f
    Signed-off-by: Pavankumar Kondeti <pkondeti@codeaurora.org>
    Signed-off-by: Adam W. Willis <return.of.octobot@gmail.com>

--- kernel/sched/core.c
+++ kernel/sched/core.c
@@ -4939,9 +4939,10 @@ unsigned int sched_lib_mask_force;
 bool is_sched_lib_based_app(pid_t pid)
 {
 	const char *name = NULL;
-	char *lib_list, *libname;
+	char *libname, *lib_list;
 	struct vm_area_struct *vma;
 	char path_buf[LIB_PATH_LENGTH];
+	char tmp_lib_name[LIB_PATH_LENGTH];
 	bool found = false;
 	struct task_struct *p;
 	struct mm_struct *mm;
@@ -4949,14 +4950,11 @@ bool is_sched_lib_based_app(pid_t pid)
 	if (strnlen(sched_lib_name, LIB_PATH_LENGTH) == 0)
 		return false;
 
-	lib_list = kstrdup(sched_lib_name, GFP_KERNEL);
-
 	rcu_read_lock();
 
 	p = find_process_by_pid(pid);
 	if (!p) {
 		rcu_read_unlock();
-		kfree(lib_list);
 		return false;
 	}
 
@@ -4976,11 +4974,14 @@ bool is_sched_lib_based_app(pid_t pid)
 			if (IS_ERR(name))
 				goto release_sem;
 
+			strlcpy(tmp_lib_name, sched_lib_name, LIB_PATH_LENGTH);
+			lib_list = tmp_lib_name;
 			while ((libname = strsep(&lib_list, ","))) {
+				libname = skip_spaces(libname);
 				if (strnstr(name, libname,
 					strnlen(name, LIB_PATH_LENGTH))) {
 					found = true;
-					break;
+					goto release_sem;
 				}
 			}
 		}
@@ -4991,7 +4992,6 @@ release_sem:
 	mmput(mm);
 put_task_struct:
 	put_task_struct(p);
-	kfree(lib_list);
 	return found;
 }
 
