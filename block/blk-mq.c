e85f06fe4be75ebebad97b20a2ea8898fc3358e5
John Galt <johngaltfirstrun@gmail.com>
2020-12-08 09:25:13 -0500

    blk-mq: fix mistake

--- block/blk-mq.c
+++ block/blk-mq.c
@@ -2435,8 +2435,6 @@ struct request_queue *blk_mq_init_allocated_queue(struct blk_mq_tag_set *set,
 
 	q->nr_queues = nr_cpu_ids;
 
-	q->queue_flags |= QUEUE_FLAG_MQ_DEFAULT;
-
 	if (!(set->flags & BLK_MQ_F_SG_MERGE))
 		q->queue_flags |= 1 << QUEUE_FLAG_NO_SG_MERGE;
 
