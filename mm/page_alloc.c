7ad4bb8c1889cc21add923089285f29ba0296708
Minchan Kim <minchan@google.com>
2021-01-20 17:33:42 -0800

    ANDROID: mm: use alloc_flags for cma first alloc policy
    rmqueue internal functions to allocate cma memory should use
    alloc_flags instead of gfp_flags because it's more restricted flags
    to be considered allocation context. Otherwise, we could allocate
    page from CMA area even though current context already disable
    cma memory allocation. For example, current allocation context is
    limited not to allocate the page from CMA area by PF_MEMALLOC_NOCMA
    to prevent longterm pin but it's ignored so the longterm pin page
    could be allocated from CMA area.
    
    Bug: 178019362
    Signed-off-by: Minchan Kim <minchan@google.com>
    Change-Id: I61bd4642c91ecd9153f6c59f89e296e8b515f1ad

--- mm/page_alloc.c
+++ mm/page_alloc.c
@@ -2919,7 +2919,7 @@ static struct page *__rmqueue_pcplist(struct zone *zone, int migratetype,
 	do {
 		/* First try to get CMA pages */
 		if (migratetype == MIGRATE_MOVABLE &&
-				gfp_flags & __GFP_CMA) {
+				alloc_flags & ALLOC_CMA) {
 			list = get_populated_pcp_list(zone, 0, pcp,
 					get_cma_migrate_type(), cold);
 		}
@@ -3006,7 +3006,7 @@ struct page *rmqueue(struct zone *preferred_zone,
 		}
 
 		if (!page && migratetype == MIGRATE_MOVABLE &&
-				gfp_flags & __GFP_CMA)
+				alloc_flags & ALLOC_CMA)
 			page = __rmqueue_cma(zone, order);
 
 		if (!page)
