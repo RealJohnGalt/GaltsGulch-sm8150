0b97ff77c8d1a85cc15d08349c666048aa4d7f6b
Mel Gorman <mgorman@techsingularity.net>
2021-01-25 08:59:06 +0000

    sched/fair: Remove SIS_AVG_CPU
    SIS_AVG_CPU was introduced as a means of avoiding a search when the
    average search cost indicated that the search would likely fail. It was
    a blunt instrument and disabled by commit 4c77b18cf8b7 ("sched/fair: Make
    select_idle_cpu() more aggressive") and later replaced with a proportional
    search depth by commit 1ad3aaf3fcd2 ("sched/core: Implement new approach
    to scale select_idle_cpu()").
    
    While there are corner cases where SIS_AVG_CPU is better, it has now been
    disabled for almost three years. As the intent of SIS_PROP is to reduce
    the time complexity of select_idle_cpu(), lets drop SIS_AVG_CPU and focus
    on SIS_PROP as a throttling mechanism.
    
    Signed-off-by: Mel Gorman <mgorman@techsingularity.net>
    Signed-off-by: Peter Zijlstra (Intel) <peterz@infradead.org>
    Reviewed-by: Vincent Guittot <vincent.guittot@linaro.org>
    Link: https://lkml.kernel.org/r/20210125085909.4600-2-mgorman@techsingularity.net
    (cherry picked from commit e6e0dc2d5497f7f3ed970052917e2923c6f453f4)
    (cherry picked from commit 2eb55ce6da794969b7f0a2413d9d7d6c10ad3872)

--- kernel/sched/features.h
+++ kernel/sched/features.h
@@ -61,8 +61,7 @@
 /*
  * When doing wakeups, attempt to limit superfluous scans of the LLC domain.
  */
-#define SCHED_FEAT_SIS_AVG_CPU 0
-#define SCHED_FEAT_SIS_PROP 0
+#define SCHED_FEAT_SIS_PROP 1
 
 /*
  * Issue a WARN when we do multiple update_rq_clock() calls
