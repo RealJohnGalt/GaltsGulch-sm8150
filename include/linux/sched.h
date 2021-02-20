b5f0ca2b6f6b4ee6ff658e65900ff26375b3f0a7
Sultan Alsawaf <sultan@kerneltoast.com>
2021-02-19 20:34:19 -0800

    sched/core: Free dead tasks asynchronously in finish_task_switch()
    Task stacks are frequently freed from finish_task_switch() during a
    context switch, in addition to the occasional task struct itself. This
    not only slows down context switches, but also wastes CPU time on high
    priority CPUs. Since unbound workqueues are now affined to the little
    CPU cluster, we can offload the task frees away from the current CPU
    entirely if it's a high-performance CPU, and defer them onto a little
    CPU. This reduces the amount of time spent in context switches and
    reclaims CPU time from more-important CPUs.
    
    Signed-off-by: Sultan Alsawaf <sultan@kerneltoast.com>

--- include/linux/sched.h
+++ include/linux/sched.h
@@ -1437,6 +1437,12 @@ struct task_struct {
 	bool utask_slave;
 #endif
 
+	struct {
+		struct work_struct work;
+		atomic_t running;
+		bool free_stack;
+	} async_free;
+
 	/*
 	 * New fields for task_struct should be added above here, so that
 	 * they are included in the randomized portion of task_struct.
