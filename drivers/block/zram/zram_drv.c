a557e15286dd50d92d9abf68568949a05f78dbef
Lup Gabriel <gwolf2u.website@gmail.com>
2021-03-08 19:59:07 +0200

    fix build after 4.14.224 this reverts commit 09beeb4694414e6dd40f947096bc07ab3fa169d4

--- drivers/block/zram/zram_drv.c
+++ drivers/block/zram/zram_drv.c
@@ -1085,7 +1085,8 @@ static ssize_t mm_stat_show(struct device *dev,
 			zram->limit_pages << PAGE_SHIFT,
 			max_used << PAGE_SHIFT,
 			(u64)atomic64_read(&zram->stats.same_pages),
-			atomic_long_read(&pool_stats.pages_compacted));
+			num_compacted,
+			(u64)atomic64_read(&zram->stats.huge_pages));
 	up_read(&zram->init_lock);
 
 	return ret;
