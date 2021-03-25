2a0cb80f91862d146f7b597d89a6bf5e006b549c
John Galt <johngaltfirstrun@gmail.com>
2021-03-25 09:21:01 -0400

    Revert "usb/f_mtp: kill all pm qos timeouts"
    This reverts commit 2c4aecd599c76a69ecb3ac212c523df2d9449704.

--- drivers/usb/gadget/function/f_mtp.c
+++ drivers/usb/gadget/function/f_mtp.c
@@ -901,10 +901,21 @@ static void send_file_work(struct work_struct *data)
 
 	mtp_log("(%lld %lld)\n", offset, count);
 
-	pm_qos_update_request(&devfreq_mtp_request, MAX_CPUFREQ - 1);
-	pm_qos_update_request(&little_cpu_mtp_freq, MAX_CPUFREQ);
-	pm_qos_update_request(&big_cpu_mtp_freq, MAX_CPUFREQ);
-	pm_qos_update_request(&big_plus_cpu_mtp_freq, MAX_CPUFREQ);
+	if (dev->xfer_file_length >= FILE_LENGTH) {
+		pm_qos_update_request(&devfreq_mtp_request, MAX_CPUFREQ - 1);
+		pm_qos_update_request(&little_cpu_mtp_freq, MAX_CPUFREQ);
+		pm_qos_update_request(&big_cpu_mtp_freq, MAX_CPUFREQ);
+		pm_qos_update_request(&big_plus_cpu_mtp_freq, MAX_CPUFREQ);
+	} else {
+		pm_qos_update_request_timeout(&devfreq_mtp_request,
+		MAX_CPUFREQ - 1, PM_QOS_TIMEOUT);
+		pm_qos_update_request_timeout(&little_cpu_mtp_freq,
+		MAX_CPUFREQ, PM_QOS_TIMEOUT);
+		pm_qos_update_request_timeout(&big_cpu_mtp_freq,
+		MAX_CPUFREQ, PM_QOS_TIMEOUT);
+		pm_qos_update_request_timeout(&big_plus_cpu_mtp_freq,
+		MAX_CPUFREQ, PM_QOS_TIMEOUT);
+	}
 
 	if (dev->xfer_send_header) {
 		hdr_size = sizeof(struct mtp_data_header);
@@ -1261,14 +1272,14 @@ static long mtp_send_receive_ioctl(struct file *fp, unsigned int code,
 	ret = dev->xfer_result;
 	if (mtp_receive_flag) {
 		mtp_receive_flag = false;
-		pm_qos_update_request(&devfreq_mtp_request,
-		MAX_CPUFREQ - 1);
-		pm_qos_update_request(&little_cpu_mtp_freq,
-		MAX_CPUFREQ);
-		pm_qos_update_request(&big_cpu_mtp_freq,
-		MAX_CPUFREQ);
-		pm_qos_update_request(&big_plus_cpu_mtp_freq,
-		MAX_CPUFREQ);
+		pm_qos_update_request_timeout(&devfreq_mtp_request,
+		MAX_CPUFREQ - 1, PM_QOS_TIMEOUT);
+		pm_qos_update_request_timeout(&little_cpu_mtp_freq,
+		MAX_CPUFREQ, PM_QOS_TIMEOUT);
+		pm_qos_update_request_timeout(&big_cpu_mtp_freq,
+		MAX_CPUFREQ, PM_QOS_TIMEOUT);
+		pm_qos_update_request_timeout(&big_plus_cpu_mtp_freq,
+		MAX_CPUFREQ, PM_QOS_TIMEOUT);
 		msm_cpuidle_set_sleep_disable(false);
 	}
 	fput(filp);
