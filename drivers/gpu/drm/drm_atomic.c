5f2bd7cb8bca3562264f9de9ef52733dc31ac1af
John Galt <johngaltfirstrun@gmail.com>
2021-04-06 13:05:42 -0400

    Revert "[FIXUP] 445e1bc ("sched/boost: Boost CPU and DDR bus when sched_boost is triggered")"
    This reverts commit 613de9e843c34b4306ef8611d35a97340b4705db.

--- drivers/gpu/drm/drm_atomic.c
+++ drivers/gpu/drm/drm_atomic.c
@@ -33,7 +33,6 @@
 #include <linux/sync_file.h>
 #include <linux/cpu_input_boost.h>
 #include <linux/devfreq_boost.h>
-#include <linux/sched/sysctl.h>
 
 #include "drm_crtc_internal.h"
 
@@ -2250,9 +2249,7 @@ int drm_mode_atomic_ioctl(struct drm_device *dev,
 			(arg->flags & DRM_MODE_PAGE_FLIP_EVENT))
 		return -EINVAL;
 
-	/* Boost CPU and DDR when committing a new frame if sched_boost > 0 */
-	if (!(arg->flags & DRM_MODE_ATOMIC_TEST_ONLY) &&
-			sysctl_sched_boost) {
+	if (!(arg->flags & DRM_MODE_ATOMIC_TEST_ONLY)) {
 		cpu_input_boost_kick();
 		devfreq_boost_kick(DEVFREQ_MSM_CPUBW);
 		devfreq_boost_kick(DEVFREQ_MSM_LLCCBW);
