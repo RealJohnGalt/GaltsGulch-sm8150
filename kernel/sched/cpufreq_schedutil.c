e4e60f66509d295385238724c8b0240c59ea4b52
John Galt <johngaltfirstrun@gmail.com>
2021-04-06 14:44:19 -0400

    sched: fixup iowait_boost

--- kernel/sched/cpufreq_schedutil.c
+++ kernel/sched/cpufreq_schedutil.c
@@ -758,8 +758,6 @@ static int sugov_init(struct cpufreq_policy *policy)
 	tunables->down_rate_limit_us =
 				cpufreq_policy_transition_delay_us(policy);
 
-	tunables->iowait_boost_enable = false;
-
 	policy->governor_data = sg_policy;
 	sg_policy->tunables = tunables;
 	stale_ns = sched_ravg_window + (sched_ravg_window >> 3);
