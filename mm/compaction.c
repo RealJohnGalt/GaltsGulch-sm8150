0f930b74fa076956eead4cab51dfa985e0032065
LibXZR <xzr467706992@163.com>
2021-04-24 23:22:09 +0800

    mm: compaction: Run ZRAM compaction on automatic compaction
    * Subsequent to "mm: compaction: Add automatic compaction mechanism"

--- mm/compaction.c
+++ mm/compaction.c
@@ -1921,6 +1921,8 @@ static void compact_nodes(void)
 		compact_node(nid);
 }
 
+void zram_compact(void);
+
 static void do_compaction(struct work_struct *work)
 {
 	/* Return early if the screen is on */
@@ -1931,6 +1933,9 @@ static void do_compaction(struct work_struct *work)
 
 	/* Do full compaction */
 	compact_nodes();
+
+	/* Do ZRAM compaction */
+	zram_compact();
 
 	/* Force compaction timeout */
 	compaction_forced_timeout = jiffies + msecs_to_jiffies(compaction_timeout_ms);
