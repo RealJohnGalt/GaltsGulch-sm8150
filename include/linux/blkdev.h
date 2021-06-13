dbd38f3a7d20df95d8c1c198431b8bdc0a528706
Panchajanya1999 <panchajanya@azure-dev.live>
2021-06-13 15:55:25 +0530

    blkdev: Do not perform IO stats
    Collecting I/O stats requires an extra overhead and hurts performance
    a bit.
    
    Change-Id: I43c940210ff97581742e6128cb44a65f9da8589d
    Signed-off-by: Panchajanya1999 <panchajanya@azure-dev.live>

--- include/linux/blkdev.h
+++ include/linux/blkdev.h
@@ -669,8 +669,7 @@ struct request_queue {
 				 (1 << QUEUE_FLAG_STACKABLE)	|	\
 				 (1 << QUEUE_FLAG_SAME_COMP))
 
-#define QUEUE_FLAG_MQ_DEFAULT	((1 << QUEUE_FLAG_IO_STAT) |		\
-				 (1 << QUEUE_FLAG_STACKABLE)	|	\
+#define QUEUE_FLAG_MQ_DEFAULT	((1 << QUEUE_FLAG_STACKABLE)	|	\
 				 (1 << QUEUE_FLAG_SAME_COMP)	|	\
 				 (1 << QUEUE_FLAG_POLL))
 
