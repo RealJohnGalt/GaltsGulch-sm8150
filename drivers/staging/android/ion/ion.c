22aafb9a5af8111ad7057794eb313f0d36341708
John Galt <johngaltfirstrun@gmail.com>
2021-06-22 11:43:54 -0400

    android/ion: fixup 9ac1aab3b7f803

--- drivers/staging/android/ion/ion.c
+++ drivers/staging/android/ion/ion.c
@@ -558,7 +558,7 @@ void ion_device_add_heap(struct ion_device *idev, struct ion_heap *heap)
 {
 	if (heap->flags & ION_HEAP_FLAG_DEFER_FREE) {
 		heap->wq = alloc_workqueue("%s", WQ_UNBOUND | WQ_MEM_RECLAIM |
-					   WQ_CPU_INTENSIVE, 0, heap->name);
+					   WQ_CPU_INTENSIVE, heap->name);
 		BUG_ON(!heap->wq);
 	}
 
