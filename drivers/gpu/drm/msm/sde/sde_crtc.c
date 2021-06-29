2de58ccae2e4c873f39b5d92bf8f38ca861b91ae
John Galt <johngaltfirstrun@gmail.com>
2021-06-29 08:26:33 -0400

    sde: also boost prime on fod press

--- drivers/gpu/drm/msm/sde/sde_crtc.c
+++ drivers/gpu/drm/msm/sde/sde_crtc.c
@@ -65,8 +65,9 @@
 #include <linux/sysfs.h>
 #include <linux/module.h>
 #include <linux/init.h>
-#include <drm/drm_mipi_dsi.h>
+#include <linux/cpu_input_boost.h>
 #include <linux/devfreq_boost.h>
+#include <drm/drm_mipi_dsi.h>
 extern int msm_drm_notifier_call_chain(unsigned long val, void *v);
 
 #define SDE_PSTATES_MAX (SDE_STAGE_MAX * 4)
@@ -3446,6 +3447,7 @@ ssize_t oneplus_display_notify_fp_press(struct device *dev,
 	ktime_t now;
 	bool need_commit = false;
 	int onscreenfp_status = 0;
+	cpu_input_boost_kick_max(1000);
 	devfreq_boost_kick_max(DEVFREQ_MSM_CPUBW, 1000);
 	devfreq_boost_kick_max(DEVFREQ_MSM_LLCCBW, 1000);
 	if ((connector == NULL) || (connector->encoder == NULL)
