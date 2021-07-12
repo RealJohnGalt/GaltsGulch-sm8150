6d53d0027b688be3fd056474e6cad50b721e09e6
John Galt <johngaltfirstrun@gmail.com>
2021-07-12 14:43:47 -0400

    mm/compaction: guard zram compaction for zram

--- mm/compaction.c
+++ mm/compaction.c
@@ -1921,7 +1921,9 @@ static void compact_nodes(void)
 		compact_node(nid);
 }
 
+#ifdef CONFIG_ZRAM
 void zram_compact(void);
+#endif
 
 static void do_compaction(struct work_struct *work)
 {
@@ -1934,8 +1936,10 @@ static void do_compaction(struct work_struct *work)
 	/* Do full compaction */
 	compact_nodes();
 
+#ifdef CONFIG_ZRAM
 	/* Do ZRAM compaction */
 	zram_compact();
+#endif
 
 	/* Force compaction timeout */
 	compaction_forced_timeout = jiffies + msecs_to_jiffies(compaction_timeout_ms);
