2c26de24bec7e6f3f0258ce3527e52251065a652
Sultan Alsawaf <sultan@kerneltoast.com>
2021-07-17 10:22:58 -0700

    msm: kgsl: Affine kgsl_3d0_irq and worker kthread to the big CPU cluster (CPU4)
    These are in the critical path for rendering frames to the display, so
    mark them as performance-critical and affine them to the big CPU
    cluster. They aren't placed onto the prime cluster because the
    single-CPU prime cluster will be used to run the DRM IRQ and kthreads.
    DRM is more latency-critical than KGSL and we need to have DRM and KGSL
    running on separate CPUs for the best performance, so KGSL gets the big
    cluster.
    
    Note that since there are other IRQs requested via kgsl_request_irq(),
    we must specify that the IRQ to be made perf-critical is kgsl_3d0_irq.
    
    Signed-off-by: Sultan Alsawaf <sultan@kerneltoast.com>
    Signed-off-by: Danny Lin <danny@kdrag0n.dev>
    
    @RealJohnGalt: update for per-core affinity and caf changes.

--- drivers/gpu/msm/kgsl.c
+++ drivers/gpu/msm/kgsl.c
@@ -4944,6 +4944,7 @@ int kgsl_device_platform_probe(struct kgsl_device *device)
 {
 	int status = -EINVAL;
 	struct resource *res;
+	unsigned long irqflags = IRQF_TRIGGER_HIGH;
 
 	status = _register_device(device);
 	if (status)
@@ -5012,8 +5013,11 @@ int kgsl_device_platform_probe(struct kgsl_device *device)
 		goto error_pwrctrl_close;
 	}
 
+	if (!strcmp(device->name, "kgsl_3d0_irq"))
+		irqflags |= IRQF_PERF_FIRST_AFFINE;
+
 	status = devm_request_irq(device->dev, device->pwrctrl.interrupt_num,
-				  kgsl_irq_handler, IRQF_TRIGGER_HIGH,
+				  kgsl_irq_handler, irqflags,
 				  device->name, device);
 	if (status) {
 		KGSL_DRV_ERR(device, "request_irq(%d) failed: %d\n",
@@ -5166,7 +5170,8 @@ static long kgsl_run_one_worker_perf(struct kthread_worker *worker,
 		struct task_struct **thread, const char *name)
 {
 	kthread_init_worker(worker);
-	*thread = kthread_run(kthread_worker_fn, worker, name);
+	*thread = kthread_run_perf_critical(cpu_perf_first_mask,
+		kthread_worker_fn, worker, name);
 	if (IS_ERR(*thread)) {
 		pr_err("unable to start %s\n", name);
 		return PTR_ERR(thread);
