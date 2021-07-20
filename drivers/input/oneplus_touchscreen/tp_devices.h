bb6517081b5863915c820c0404a9fd2bb97155ed
John Galt <johngaltfirstrun@gmail.com>
2021-07-20 07:56:10 -0400

    input/oneplus_touchscreen: reimport

--- drivers/input/oneplus_touchscreen/tp_devices.h
+++ drivers/input/oneplus_touchscreen/tp_devices.h
@@ -1,38 +1,37 @@
  /***********************************************************
  * Description : OnePlus touchpanel driver
+ * 
+ * File        : tp_devices.h      
  *
- * File        : tp_devices.h
- *
- * Function    : touchpanel public interface
- *
- * Version     : V1.0
+ * Function    : touchpanel public interface  
+ * 
+ * Version     : V1.0 
  *
  ***********************************************************/
-#ifndef OPPO_TP_DEVICES_H
-#define OPPO_TP_DEVICES_H
+#ifndef OP_TP_DEVICES_H
+#define OP_TP_DEVICES_H
 //device list define
-typedef enum tp_dev{
-    TP_OFILM,
-    TP_BIEL,
-    TP_TRULY,
-    TP_BOE,
-    TP_G2Y,
-    TP_TPK,
-    TP_JDI,
-    TP_SAMSUNG,
-    TP_DSJM,
-    TP_BOE_B8,
-    TP_INNOLUX,
-    TP_HIMAX_DPT,
-    TP_AUO,
-    TP_DEPUTE,
-    TP_UNKNOWN,
-}tp_dev;
+typedef enum tp_dev {
+	TP_OFILM,
+	TP_BIEL,
+	TP_TRULY,
+	TP_BOE,
+	TP_G2Y,
+	TP_TPK,
+	TP_JDI,
+	TP_SAMSUNG,
+	TP_DSJM,
+	TP_BOE_B8,
+	TP_INNOLUX,
+	TP_HIMAX_DPT,
+	TP_AUO,
+	TP_DEPUTE,
+	TP_UNKNOWN,
+} tp_dev;
 
 struct tp_dev_name {
-    tp_dev type;
-    char name[32];
+	tp_dev type;
+	char name[32];
 };
 
 #endif
-
