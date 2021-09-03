9d5fcb47dab7bc53b5ff962a4717d3f312cbd1f7
John Galt <johngaltfirstrun@gmail.com>
2021-09-03 18:22:29 -0400

    Revert "gulch_defconfig: disable cpu input boosting"
    This reverts commit 36236c196372c34439e7c6e6832026535b33eaec.

--- drivers/gpu/drm/drm_atomic.c
+++ drivers/gpu/drm/drm_atomic.c
@@ -2250,7 +2250,7 @@ static int __drm_mode_atomic_ioctl(struct drm_device *dev, void *data,
 			(arg->flags & DRM_MODE_PAGE_FLIP_EVENT))
 		return -EINVAL;
 
-	if (!(arg->flags & DRM_MODE_ATOMIC_TEST_ONLY && !MSM_DRM_BLANK_UNBLANK_CUST)) {
+	if (!(arg->flags & DRM_MODE_ATOMIC_TEST_ONLY)) {
 		devfreq_boost_kick(DEVFREQ_MSM_CPUBW);
 		devfreq_boost_kick(DEVFREQ_MSM_LLCCBW);
 	}
