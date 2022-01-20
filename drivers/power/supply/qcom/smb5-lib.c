32406f2a9928950989fa08b1cbfef0dc0311336e
John Galt <johngaltfirstrun@gmail.com>
2022-01-20 15:58:29 -0500

    Revert "power/supply/qcom/smb5-lib: improve f14c4e8101d"
    This reverts commit 12a97fe0c31578c702b0979b5d19eeef000a8b7f.

--- drivers/power/supply/qcom/smb5-lib.c
+++ drivers/power/supply/qcom/smb5-lib.c
@@ -7997,8 +7997,8 @@ static void set_usb_switch(struct smb_charger *chg, bool enable)
 
 	if (chg->pd_active) {
 		pr_info("%s:pd_active return\n", __func__);
-		//Will work with any accessories that don't use high
-		if (chg->typec_mode == POWER_SUPPLY_TYPEC_SOURCE_HIGH)
+		if (chg->typec_mode == POWER_SUPPLY_TYPEC_SOURCE_HIGH ||
+				chg->typec_mode == POWER_SUPPLY_TYPEC_SOURCE_MEDIUM)
 			chg->disconnect_pd = true;
 		return;
 	}
