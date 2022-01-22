57726de0588d2e0414bc0b01c3b0cdf98d945a14
John Galt <johngaltfirstrun@gmail.com>
2022-01-22 17:23:32 -0500

    zram_drv.c: hardcode to 9216
    1.5 * 6G == 9G. Best overall zram configuration for us. Note: this size
      is pre compression.
    
    Especially as we pursue faster zram with less expensive compression,
    we desire larger zram.

--- drivers/block/zram/zram_drv.c
+++ drivers/block/zram/zram_drv.c
@@ -1729,7 +1729,7 @@ static ssize_t disksize_store(struct device *dev,
 	struct zram *zram = dev_to_zram(dev);
 	int err;
 
-	disksize = memparse(buf, NULL);
+	disksize = (u64)9216 * SZ_1M;
 	if (!disksize)
 		return -EINVAL;
 
