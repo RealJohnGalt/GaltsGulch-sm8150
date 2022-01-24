5decb5ce5782683020bb5a96ddef3d5eff200b37
John Galt <johngaltfirstrun@gmail.com>
2022-01-23 23:26:52 -0500

    Revert "Partially revert "drm: Handle dim for udfps""
    This reverts commit 94d826da15fc47a116e04fe933b2824b536665cb.

--- drivers/gpu/drm/msm/dsi-staging/dsi_panel.c
+++ drivers/gpu/drm/msm/dsi-staging/dsi_panel.c
@@ -5147,8 +5147,9 @@ bool aod_complete;
 bool real_aod_mode;
 
 extern bool oneplus_dimlayer_hbm_enable;
+bool backup_dimlayer_hbm = false;
 extern int oneplus_dim_status;
-
+int backup_dim_status = 0;
 int dsi_panel_enable(struct dsi_panel *panel)
 {
 	int rc = 0;
@@ -5192,8 +5193,12 @@ int dsi_panel_enable(struct dsi_panel *panel)
 	}
 
 	panel->panel_initialized = true;
-	pr_err("dsi_panel_enable aod_mode =%d\n",panel->aod_mode);
 	oneplus_panel_status = 2; // DISPLAY_POWER_ON
+	pr_err("dsi_panel_enable aod_mode =%d\n",panel->aod_mode);
+
+	oneplus_dimlayer_hbm_enable = backup_dimlayer_hbm;
+	oneplus_dim_status = backup_dim_status;
+	pr_err("Restore dim when panel goes on");
 
 	blank = MSM_DRM_BLANK_UNBLANK_CHARGE;
 	notifier_data.data = &blank;
