cfec5b51f08a98e67cc0a27f3361776b0a6c4ba0
John Galt <johngaltfirstrun@gmail.com>
2022-03-13 13:05:22 -0400

    fs/exec: move hwc to big cluster
    Improving irq balancing.

--- fs/exec.c
+++ fs/exec.c
@@ -1865,8 +1865,8 @@ static int do_execveat_common(int fd, struct filename *filename,
 		else if (unlikely(!strncmp(filename->name,
 					   HWCOMPOSER_BIN_PREFIX,
 					   strlen(HWCOMPOSER_BIN_PREFIX)))) {
-			current->pc_flags |= PC_PRIME_AFFINE;
-			set_cpus_allowed_ptr(current, cpu_prime_mask);
+			current->pc_flags |= PC_PERF_AFFINE;
+			set_cpus_allowed_ptr(current, cpu_perf_mask);
 		} else if (unlikely(!strncmp(filename->name,
 					   UDFPS_BIN_PREFIX,
 					   strlen(UDFPS_BIN_PREFIX)))) {
