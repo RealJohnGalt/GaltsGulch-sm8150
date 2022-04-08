e3c0504681b4f357f10ddfb5ddd686e09852b495
Minchan Kim <minchan@google.com>
2022-04-08 14:29:47 -0700

    ANDROID: mm: freeing MIGRATE_ISOLATE page instantly
    Since Android has pcp list for MIGRATE_CMA[1], it could cause
    CMA allocation latency due to not freeing the MIGRATE_ISOLATE
    page immediately.
    
    Originally, MIGRATE_ISOLATED page is supposed to go buddy list
    with skipping pcp list. Otherwise, the page could be reallocated
    from pcp list or staying on the pcp list until the pcp is drained
    so that CMA keeps retrying since it couldn't find the freed page
    from buddy list. That worked before since the CMA pfnblocks changed
    only from MIGRATE_CMA to MIGRATE_ISOLATE and free function logic
    in page allocator has checked MIGRATE_ISOLATEness on every CMA
    pages using below.
    
      free_unref_page_commit
        if (migratetype >= MIGRATE_PCPTYPES)
          if(is_migrate_isolate(migratetype))
            free_one_page(page);
    
    It worked since enum MIGRATE_CMA was bigger than enum
    MIGRATE_PCPTYPES but since [1], the enum MIGRATE_CMA is less than
    MIGRATE_PCPTYPES so the logic above doesn't work any more.
    
    It could cause following race
    
             CPU 0	                          CPU 1
      free_unref_page
      migratetype = get_pfnblock_migratetype()
      set_pcppage_migratetype(MIGRATE_CMA)
    
                                    cma_alloc
    				alloc_contig_range
                                  	set_migrate_isolate(MIGRATE_ISOLATE)
      add the page into pcp list
      the page could be reallocated
    
    This patch couldn't fix the race completely due to missing zone->lock
    in order-0 page free(for performance reason). However, it's not a new
    problem so we need to deal with the issue separately.
    
    [1] ANDROID: mm: add cma pcp list
    
    Bug: 218731671
    Signed-off-by: Minchan Kim <minchan@google.com>
    Change-Id: Ibea20085ce5bfb4b74b83b041f9bda9a380120f9

--- mm/page_alloc.c
+++ mm/page_alloc.c
@@ -2769,18 +2769,19 @@ void free_hot_cold_page(struct page *page, bool cold)
 	__count_vm_event(PGFREE);
 
 	/*
-	 * We only track unmovable, reclaimable and movable on pcp lists.
+	 * We only track unmovable, reclaimable, movable and cma on pcp lists.
 	 * Free ISOLATE pages back to the allocator because they are being
 	 * offlined but treat HIGHATOMIC as movable pages so we can get those
 	 * areas back if necessary. Otherwise, we may have to free
 	 * excessively into the page allocator
 	 */
-	if (migratetype >= MIGRATE_PCPTYPES) {
+	if (migratetype >= MIGRATE_CMA) {
 		if (unlikely(is_migrate_isolate(migratetype))) {
 			free_one_page(zone, page, pfn, 0, migratetype);
 			goto out;
 		}
-		migratetype = MIGRATE_MOVABLE;
+		if (migratetype == MIGRATE_HIGHATOMIC)
+			migratetype = MIGRATE_MOVABLE;
 	}
 
 	pcp = &this_cpu_ptr(zone->pageset)->pcp;
