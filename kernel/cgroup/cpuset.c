796a90686217f1e84d651b29b7dd1be60c0028df
John Galt <johngaltfirstrun@gmail.com>
2022-04-13 11:02:45 -0400

    Revert "uclamp_assist: update values"
    This reverts commit 4bd6edaadb003604d47f31a12fd7c41d4b0dfb1d.
    
    Only useful for demonstration.

--- kernel/cgroup/cpuset.c
+++ kernel/cgroup/cpuset.c
@@ -2080,12 +2080,12 @@ static void uclamp_set(struct kernfs_open_file *of,
 	const char *cs_name = cs->css.cgroup->kn->name;
 
 	static struct ucl_param tgts[] = {
-		{"top-app",    	     	"10", "100", 1, 1},
-		{"foreground", 	     	"0",  "60",  1, 1},
+		{"top-app",    	     	"10", "100", 1, 0},
+		{"foreground", 	     	"0",  "50",  1, 1},
 		{"background", 	     	"20", "100", 0, 0},
 		{"system-background", 	"0",  "40",  0, 0},
-		{"camera-daemon",	"60", "100", 1, 1},
-		{"display",		"40", "100", 1, 1},
+		{"camera-daemon",	"10", "100", 1, 1},
+		{"display",		"20", "100", 1, 1},
 		{"restricted",		"0",  "40",  0, 0},
 	};
 
