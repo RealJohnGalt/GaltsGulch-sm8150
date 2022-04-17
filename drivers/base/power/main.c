fb005c803955dee4fe8ae04d452ea14bb45aad4d
Sultan Alsawaf <sultan@kerneltoast.com>
2022-04-17 10:53:19 -0700

    Revert "cpuidle: don't disable cpuidle when entering suspend"
    This reverts commit 0b3cc77678b9500f240c9a550f74302d0f5760da.
    
    The workflow of s2idle requires the cpuidle_resume() in dpm_noirq_end(),
    otherwise cpuidle never gets re-enabled after exiting s2idle.
    
    Signed-off-by: Sultan Alsawaf <sultan@kerneltoast.com>

--- drivers/base/power/main.c
+++ drivers/base/power/main.c
@@ -32,6 +32,7 @@
 #include <linux/suspend.h>
 #include <trace/events/power.h>
 #include <linux/cpufreq.h>
+#include <linux/cpuidle.h>
 #include <linux/timer.h>
 #include <linux/wakeup_reason.h>
 
@@ -690,6 +691,7 @@ void dpm_noirq_end(void)
 {
 	resume_device_irqs();
 	device_wakeup_disarm_wake_irqs();
+	cpuidle_resume();
 }
 
 /**
@@ -1219,6 +1221,7 @@ static int device_suspend_noirq(struct device *dev)
 
 void dpm_noirq_begin(void)
 {
+	cpuidle_pause();
 	device_wakeup_arm_wake_irqs();
 	suspend_device_irqs();
 }
