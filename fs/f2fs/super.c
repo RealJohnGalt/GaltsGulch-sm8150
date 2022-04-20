347a26eaaec395b42cd37c31126beafa0f222f95
John Galt <johngaltfirstrun@gmail.com>
2022-04-20 10:02:28 -0400

    f2fs/super: fix merge issue
    This warning isn't as an error, so was missed.

--- fs/f2fs/super.c
+++ fs/f2fs/super.c
@@ -1531,9 +1531,9 @@ static void f2fs_umount_end(struct super_block *sb, int flags)
 			struct cp_control cpc = {
 				.reason = CP_UMOUNT,
 			};
-			down_write(&sbi->gc_lock);
+			f2fs_down_write(&sbi->gc_lock);
 			f2fs_write_checkpoint(F2FS_SB(sb), &cpc);
-			up_write(&sbi->gc_lock);
+			f2fs_up_write(&sbi->gc_lock);
 		}
 	}
 }
@@ -2698,15 +2698,15 @@ int f2fs_quota_sync(struct super_block *sb, int type)
 	/*
 	 * do_quotactl
 	 *  f2fs_quota_sync
-	 *  down_read(quota_sem)
+	 *  f2fs_down_read(quota_sem)
 	 *  dquot_writeback_dquots()
 	 *  f2fs_dquot_commit
 	 *                            block_operation
-	 *                            down_read(quota_sem)
+	 *                            f2fs_down_read(quota_sem)
 	 */
 	f2fs_lock_op(sbi);
 
-	down_read(&sbi->quota_sem);
+	f2fs_down_read(&sbi->quota_sem);
 	ret = dquot_writeback_dquots(sb, type);
 	if (ret)
 		goto out;
@@ -2744,7 +2744,7 @@ int f2fs_quota_sync(struct super_block *sb, int type)
 out:
 	if (ret)
 		set_sbi_flag(F2FS_SB(sb), SBI_QUOTA_NEED_REPAIR);
-	up_read(&sbi->quota_sem);
+	f2fs_up_read(&sbi->quota_sem);
 	f2fs_unlock_op(sbi);
 	return ret;
 }
