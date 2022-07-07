5bee6e8466b83243a3d7b44f19e5bb612d94ae39
John Galt <johngaltfirstrun@gmail.com>
2022-07-07 13:49:27 -0400

    smb5-lib: revert old PD hacks
    These didn't even fix all scenarios, and there's a better solution. Make
    standalone for better clean picking.

--- drivers/power/supply/qcom/smb5-lib.c
+++ drivers/power/supply/qcom/smb5-lib.c
@@ -7995,21 +7995,6 @@ static void set_usb_switch(struct smb_charger *chg, bool enable)
 		return;
 	}
 
-	if (chg->real_charger_type == POWER_SUPPLY_TYPE_USB) {
-		pr_debug("%s:OP FIXUP: early return for passthrough\n", __func__);
-		return;
-	}
-
-	if (chg->pd_active) {
-		if (chg->typec_mode == POWER_SUPPLY_TYPEC_SINK ||
-				chg->typec_mode == POWER_SUPPLY_TYPEC_SINK_DEBUG_ACCESSORY ||
-				chg->typec_mode == POWER_SUPPLY_TYPEC_SINK_AUDIO_ADAPTER ||
-				chg->typec_mode == POWER_SUPPLY_TYPEC_SINK_POWERED_CABLE) {
-			pr_debug("%s:OP FIXUP: pd_active return\n", __func__);
-			return;
-		}
-	}
-
 	if (enable) {
 		pr_debug("switch on fastchg\n");
 		chg->switch_on_fastchg = true;
