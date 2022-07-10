ef4cec8bafa2aa58d5855534b37af2d305a593e4
John Galt <johngaltfirstrun@gmail.com>
2022-07-10 10:38:40 -0400

    android/binder: squash revert 5.10 merge

--- drivers/android/binderfs.c
+++ drivers/android/binderfs.c
@@ -360,7 +360,6 @@ static const struct super_operations binderfs_super_ops = {
 static inline bool is_binderfs_control_device(const struct dentry *dentry)
 {
 	struct binderfs_info *info = dentry->d_sb->s_fs_info;
-
 	return info->control_dentry == dentry;
 }
 
@@ -555,7 +554,6 @@ out:
 	return dentry;
 }
 
-#ifdef CONFIG_ANDROID_BINDER_LOGS
 static struct dentry *binderfs_create_dir(struct dentry *parent,
 					  const char *name)
 {
@@ -654,19 +652,13 @@ static int init_binder_logs(struct super_block *sb)
 out:
 	return ret;
 }
-#else
-static inline int init_binder_logs(struct super_block *sb)
-{
-	return 0;
-}
-#endif
 
 static int binderfs_fill_super(struct super_block *sb, void *data, int silent)
 {
 	int ret;
 	struct binderfs_info *info;
 	struct inode *inode = NULL;
-	struct binderfs_device device_info = {};
+	struct binderfs_device device_info = { { 0 } };
 	const char *name;
 	size_t len;
 
