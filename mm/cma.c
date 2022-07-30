0382ea593d045c13e878a5b408d19d1e5ccc48fb
Kazuki Hashimoto <kazukih@tuta.io>
2022-07-31 03:49:55 +0900

    Revert "mm: cma: Increase retries if less blocks available"
    This reverts commit 5a883fb75e3aa713c4e59ca78ab9acd09b78890e.
    
    Signed-off-by: Kazuki Hashimoto <kazukih@tuta.io>

--- mm/cma.c
+++ mm/cma.c
@@ -463,8 +463,6 @@ struct page *cma_alloc(struct cma *cma, size_t count, unsigned int align,
 	struct page *page = NULL;
 	int ret = -ENOMEM;
 	int retry_after_sleep = 0;
-	int max_retries = 2;
-	int available_regions = 0;
 
 	if (!cma || !cma->count)
 		return NULL;
@@ -491,15 +489,8 @@ struct page *cma_alloc(struct cma *cma, size_t count, unsigned int align,
 				bitmap_maxno, start, bitmap_count, mask,
 				offset);
 		if (bitmap_no >= bitmap_maxno) {
-			if (retry_after_sleep < max_retries) {
+			if (retry_after_sleep < 2) {
 				start = 0;
-				/*
-				 * update max retries if available free regions
-				 * are less.
-				 */
-				if (available_regions < 3)
-					max_retries = 5;
-				available_regions = 0;
 				/*
 				 * Page may be momentarily pinned by some other
 				 * process which has been scheduled out, eg.
@@ -517,8 +508,6 @@ struct page *cma_alloc(struct cma *cma, size_t count, unsigned int align,
 				break;
 			}
 		}
-
-		available_regions++;
 		bitmap_set(cma->bitmap, bitmap_no, bitmap_count);
 		/*
 		 * It's safe to drop the lock here. We've marked this region for
