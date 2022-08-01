21a67b4238472b681a72014d7ebcb7b8af5dca3d
John Galt <johngaltfirstrun@gmail.com>
2022-08-01 10:29:48 -0400

    Revert "random: always use non-blocking pool"
    This reverts commit 760adedefb6eae9ebfce2ad7c245d06b0cb7bf2f.

--- drivers/char/random.c
+++ drivers/char/random.c
@@ -1867,6 +1867,12 @@ _random_read(int nonblock, char __user *buf, size_t nbytes)
 }
 
 static ssize_t
+random_read(struct file *file, char __user *buf, size_t nbytes, loff_t *ppos)
+{
+	return _random_read(file->f_flags & O_NONBLOCK, buf, nbytes);
+}
+
+static ssize_t
 urandom_read(struct file *file, char __user *buf, size_t nbytes, loff_t *ppos)
 {
 	unsigned long flags;
@@ -2009,7 +2015,7 @@ static int random_fasync(int fd, struct file *filp, int on)
 }
 
 const struct file_operations random_fops = {
-	.read  = urandom_read,
+	.read  = random_read,
 	.write = random_write,
 	.poll  = random_poll,
 	.unlocked_ioctl = random_ioctl,
