3ced6657f7e71e4859c0fadcda30eb5feaadedad
John Galt <johngaltfirstrun@gmail.com>
2022-10-25 17:15:56 -0400

    Revert "uclamp assist: stop boosting top app"
    This reverts commit 87a0cec7af984597d49d55176b79f42147a7b644.

--- kernel/cgroup/cpuset.c
+++ kernel/cgroup/cpuset.c
@@ -2133,7 +2133,7 @@ static void uclamp_set(struct kernfs_open_file *of,
 	const char *cs_name = cs->css.cgroup->kn->name;
 
 	static struct ucl_param tgts[] = {
-		{"top-app",    	     	"10", "100", 1, 0},
+		{"top-app",    	     	"10", "100", 1, 1},
 		{"foreground", 	     	"10", "80",  1, 0},
 		{"background", 	     	"20", "100", 0, 0},
 		{"system-background", 	"0",  "40",  0, 0},
