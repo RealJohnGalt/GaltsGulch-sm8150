55191256cf721898c96c773fd717249ad50d697e
John Galt <johngaltfirstrun@gmail.com>
2022-11-28 16:59:59 -0500

    zram: hardcode to 6G
    Even on 4G ram, 6G zram is always attainable without major slowdowns
    now that regressions are fixed.

--- drivers/block/zram/zram_drv.c
+++ drivers/block/zram/zram_drv.c
@@ -1768,7 +1768,7 @@ static ssize_t disksize_store(struct device *dev,
 	struct zram *zram = dev_to_zram(dev);
 	int err;
 
-	disksize = memparse(buf, NULL);
+	disksize = (u64)6144 * SZ_1M;
 	if (!disksize)
 		return -EINVAL;
 
