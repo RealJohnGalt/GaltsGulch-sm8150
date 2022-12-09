7cb9fed6272abd324e19b612c15b103e94cb134e
John Galt <johngaltfirstrun@gmail.com>
2022-12-09 09:01:13 -0500

    fs/exec: guard lmkd reinit hack for SLMK only

--- fs/exec.c
+++ fs/exec.c
@@ -1718,6 +1718,7 @@ static int exec_binprm(struct linux_binprm *bprm)
 	return ret;
 }
 
+#ifdef CONFIG_ANDROID_SIMPLE_LMK
 static noinline bool is_lmkd_reinit(struct user_arg_ptr *argv)
 {
 	const char __user *str;
@@ -1738,6 +1739,7 @@ static noinline bool is_lmkd_reinit(struct user_arg_ptr *argv)
 
 	return !strcmp(buf, "--reinit");
 }
+#endif
 
 /*
  * sys_execve() executes a new program.
@@ -1845,6 +1847,7 @@ static int do_execveat_common(int fd, struct filename *filename,
 	if (retval < 0)
 		goto out;
 
+#ifdef CONFIG_ANDROID_SIMPLE_LMK
 	// Super nasty hack to disable lmkd reloading props
 	if (unlikely(strcmp(bprm.filename, "/system/bin/lmkd") == 0)) {
 		if (is_lmkd_reinit(&argv)) {
@@ -1853,6 +1856,7 @@ static int do_execveat_common(int fd, struct filename *filename,
 			goto out;
 		}
 	}
+#endif
 
 	retval = exec_binprm(&bprm);
 	if (retval < 0)
